9aacaac4fd31504a4981edd7451fd3ed27a9cbb8
Nareshsagar7989 <kondikantinareshsagar7989@gmail.com>
2025-09-22 11:01:55 +0530

    non primitive datatypes

--- datatype.js
+++ datatype.js
@@ -2,3 +2,22 @@ a = 7897;
 console.log(typeof a);
 name = "naresh";
 console.log(typeof name);
+isLoggedIn = false;
+console.log(typeof isLoggedIn);
+a = null;
+console.log(typeof a);
+d = undefined;
+console.log(typeof d);
+
+
+//non primitive
+//array,object,function
+arr = [1,2,3,4,5];
+console.log(typeof arr);
+obj = {name:"naresh",age:23};
+console.log(typeof obj);
+function sum(){
+    return 0;
+}
+console.log(typeof sum);
+console.log(typeof Date);
