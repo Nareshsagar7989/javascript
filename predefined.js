719dd1806068207190f07213db1d0a1afa06f446
Nareshsagar7989 <kondikantinareshsagar7989@gmail.com>
2025-09-22 16:02:03 +0530

    developed calculator

---
+++ predefined.js
@@ -0,0 +1,22 @@
+var a = Number(prompt("Enter value a:"));
+var b = Number(prompt("Enter value b:"));
+document.write(`you entered ${a} and ${b}`);
+
+function addition(){
+    var c = a + b;
+    alert(`The addition of ${a} and ${b} is ${c}`);
+
+}
+function subtraction(){
+    var c = a - b;
+    document.write(alert(`The subtraction of ${a} and ${b} is ${c}`));
+}                   
+function multiplication(){      
+   alert(`The multiplication of ${a} and ${b} is ${c}`);
+}   
+function division(){  
+    var c = a / b;
+    alert(`The division of ${a} and ${b} is ${c}`);
+}
+
+//predefined functions
