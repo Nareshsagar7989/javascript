e6bf6d59ae8cf7e879532a2a6d6080c42ce5c1af
Nareshsagar7989 <kondikantinareshsagar7989@gmail.com>
2025-09-22 10:29:23 +0530

    types of variables

---
+++ app.js
@@ -0,0 +1,15 @@
+//types of variables
+//var, let, const   
+//let and const are block scoped
+//var is function scoped
+//re-declaration is not allowed in let and const
+//re-declaration is allowed in var
+//re-assignment is allowed in let and var
+//re-assignment is not allowed in const
+var a = 70;
+var a = 90; 
+var a = 100;    
+console.log(a);
+let b = 50; 
+const c
+console.log(`value of b is ${b}`);
