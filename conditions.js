e361b0b7b639365c4ad4994682608ddb02cc64e9
Nareshsagar7989 <kondikantinareshsagar7989@gmail.com>
2025-09-22 16:20:51 +0530

    conditional statements

---
+++ conditions.js
@@ -0,0 +1,9 @@
+age = Number(prompt("enter the age"));
+
+if (age < 18){
+    document.write("you are not eligible to use dashboard");
+}
+else(age > 18){
+    document.write("you are eligible to use dashboard");
+}
+
